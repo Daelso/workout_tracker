3d64224a6d97034defa40b3296cfcc76934fa5d6
Chase Murtaugh <cmurtau@g.clemson.edu>
2021-11-01 17:38:03 -0400

    Reworks to async

--- routes/api.js
+++ routes/api.js
@@ -4,24 +4,28 @@ const router = require("express").Router();
 
 // GET Request for getting all workouts
 router.get("/api/workouts", async(req, res) => {
-	db.Workout.find()
-		.then((dbData) => {
-			res.json(dbData);
-		})
-		.catch((err) => {
+	try{
+	const findAllWorkouts = await db.Workout.find()
+
+		res.json(findAllWorkouts);
+		
+	}
+		catch(err) {
 			res.json(err);
-		});
+		};
 });
 
 // GET request for the stats page.
-router.get("/api/workouts/range", (req, res) => {
-	db.Workout.find()
-		.then((dbData) => {
-			res.json(dbData);
-		})
-		.catch((err) => {
+router.get("/api/workouts/range", async (req, res) => {
+try{
+	const findWorkoutRange = await db.Workout.find()
+
+			res.json(findWorkoutRange);
+		}
+
+		catch (err) {
 			res.json(err);
-		});
+		};
 });
 
 
