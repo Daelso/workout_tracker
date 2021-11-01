c14b2b619cca4e7ae3d828a2d7b0255775ed922f
Chase Murtaugh <cmurtau@g.clemson.edu>
2021-11-01 16:58:52 -0400

    Following deployment tutorial

--- Develop/server.js
+++ Develop/server.js
@@ -10,14 +10,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+//Will one day allow for deployment, or it defaults to local host.
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/homeroutes"));
-app.use(require("./routes/api"));
+app.use(require("./routes/homeroutes")); //points to our homeroutes/html pages
+app.use(require("./routes/api")); //points to our new api routes
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
