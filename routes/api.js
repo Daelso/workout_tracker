eaaeea0bf67d37587e37cb4442340f6a0b7486e4
Chase Murtaugh <cmurtau@g.clemson.edu>
2021-11-01 18:15:52 -0400

    Post and update requests, can't get them async though, sadly

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ const router = require("express").Router();
 // GET Request for getting all workouts
 router.get("/api/workouts", async(req, res) => {
 	try{
-	const findAllWorkouts = await db.Workout.find()
+	const findAllWorkouts = await db.Workout.find({}).sort({ date: -1})
 
 		res.json(findAllWorkouts);
 		
@@ -18,7 +18,7 @@ router.get("/api/workouts", async(req, res) => {
 // GET request for the stats page.
 router.get("/api/workouts/range", async (req, res) => {
 try{
-	const findWorkoutRange = await db.Workout.find()
+	const findWorkoutRange = await db.Workout.find({}).sort({ date: -1})
 
 			res.json(findWorkoutRange);
 		}
@@ -28,8 +28,35 @@ try{
 		};
 });
 
+router.post("/api/workouts", (req, res) => {
+	db.Workout.create(req.body)
+	  .then((workout) => {
+		res.status(201).json(workout);
+	  })
+	  .catch((err) => {
+		res.status(400).json(err);
+	  });
+  });
 
-
+router.put("/api/workouts/:id", async (req, res) => {
+	const id = req.params.id;
+	const body = req.body;
+	db.Workout.updateOne(
+	  { _id: id },
+	  {
+		$push: {
+		  exercises: { ...body },
+		},
+	  }
+	)
+	  .then((workout) => {
+		res.status(200).json(workout);
+	  })
+	  .catch((err) => {
+		res.status(400).json(err);
+	  });
+  });
+  
 
 
 // Export HTML routes
